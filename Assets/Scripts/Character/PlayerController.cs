4c9653f243727bcc55ff3e59ad86233f9bc25534
Evgeny <hram.ir52@gmail.com>
2023-11-28 21:38:45 +0300

    Controller player
    Скрипт для управления персонажа.

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -2,59 +2,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    public int maxHealth = 100;
+    public float speed = 5f; // Скорость движения персонажа
+    public float jumpForce = 10f; // Сила прыжка
+    private bool isGrounded; // Флаг, определяющий, находится ли персонаж на земле
 
-    private int currentHealth;
-    private bool isGrounded;
     private Rigidbody2D rb;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
     }
 
-    void Update()
+    private void Update()
     {
-        // Управление движением
+        // Проверяем, находится ли персонаж на земле
+        isGrounded = Physics2D.OverlapCircle(transform.position, 1f, LayerMask.GetMask("Ground"));
+
+        // Обработка ввода для движения по горизонтали
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontalInput, 0f);
-        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+        Vector2 movement = new Vector2(horizontalInput * speed, rb.velocity.y);
+        rb.velocity = movement;
 
-        // Прыжок по пробелу
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        // Проверка, находится ли персонаж на земле
-        isGrounded = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("Ground"));
-        if(isGrounded)
+        // Обработка прыжка
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             Debug.Log("Земля");
+            Jump();
         }
     }
 
-    public void TakeDamage(int damage)
+    private void Jump()
     {
-        currentHealth -= damage;
-
-        // Если здоровье опускается до нуля, можно выполнить дополнительные действия, например, вызвать метод GameOver() или уничтожить объект.
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    void Die()
-    {
-        // Дополнительные действия при смерти персонажа
-        Destroy(gameObject);
-        // Или другие действия...
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
